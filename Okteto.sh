a2e23c08e08ee524a711e53a4bfdb222fd585157
314257smcag2 <98874964+314257smcag2@users.noreply.github.com>
2023-01-02 17:58:11 +0100

    Update Okteto.sh

--- Okteto.sh
+++ Okteto.sh
@@ -19,4 +19,18 @@ code-server --bind-addr 127.0.0.1:12345 >> vscode.log &
 rm -rf code-server_4.9.1_amd64.deb
 apt-get update && apt-get upgrade -y 
 cat /var/lib/tor/hidden_service/hostname && sed -n '3'p ~/.config/code-server/config.yaml
-sleep 30d
+
+apt install firefox mate-system-monitor  git lxde tightvncserver wget   -y
+wget https://github.com/novnc/noVNC/archive/refs/tags/v1.2.0.tar.gz
+tar -xvf v1.2.0.tar.gz
+mkdir  /root/.vnc
+echo 'uncleluo' | vncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
+cp /noVNC-1.2.0/vnc.html /noVNC-1.2.0/index.html
+echo 'cd /root' >>/luo.sh
+echo "su root -l -c 'vncserver :2000 ' "  >>/luo.sh
+echo 'cd /noVNC-1.2.0' >>/luo.sh
+echo './utils/launch.sh  --vnc localhost:7900 --listen 80 ' >>/luo.sh
+echo root:laoluoshushu|chpasswd
+chmod 755 /luo.sh
+./luo.sh
